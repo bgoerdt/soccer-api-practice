953867d659b593159e14495d085dd4a2c20e0910
Torre Gerritsen <torre.gerritsen@pioneer.com>
2014-08-07 09:25:24 -0500

    Updated to use $scope

--- SoccerAPI/SoccerAPI/Scripts/app.js
+++ SoccerAPI/SoccerAPI/Scripts/app.js
@@ -1,59 +1,56 @@
-﻿(function () {
-	var app = angular.module('soccer', []);
+﻿var app = angular.module('soccer', []);
 	
-	app.controller('SoccerController', [ '$http', function ($http) {
-		var soccer = this;
-		soccer.teams = [];
+app.controller('SoccerController', [ '$scope', '$http', function ($scope, $http) {
+	$scope.teams = [];
 
-		$http.get('/api/Teams').success(function (data) {
-			soccer.teams = data;
-		});
-	}]);
-
-	app.controller('PlayerController', ['$http', function ($http) {
-		var playerCtrl = this;
-		playerCtrl.players = [];
-
-		$http.get('/api/Players').success(function (data) {
-			playerCtrl.players = data;
-		});
-	}]);
-
-	app.controller("CommentController", ['$http', function ($http) {
-		var commentCtrl = this;
-		commentCtrl.comments = [];
-		commentCtrl.comment = {};
-		commentCtrl.message = "n/a";
-
-		$http.get('/api/Comments').success(function (data) {
-			commentCtrl.comments = data;
-		});
-
-		this.getComments = function () {
-			$http.get('/api/Comments').success(function (data) {
-				commentCtrl.comments = data;
-			});
-		};
-
-		this.addComment = function (team) {
-			commentCtrl.comment.TeamId = team.Id;
-			var comm = commentCtrl.comment;
-			$http.post('/api/Comments', JSON.stringify(comm)).success(function (data) {
-				commentCtrl.comment = {};
-				commentCtrl.getComments();
-			});
-		};
-	}]);
-
-	app.controller("TabController", function () {
-		this.tab = 1;
-		
-		this.selectTab = function(setTab) {
-			this.tab = setTab;
-		};
-
-		this.isSelected = function (checkTab) {
-			return this.tab === checkTab;
-		};
+	$http.get('/api/Teams').success(function (data) {
+		$scope.teams = data;
+		alert("teams loaded");
 	});
-})();
+}]);
+
+app.controller('PlayerController', [ '$scope', '$http', function ($scope, $http) {
+	$scope.players = [];
+
+	$http.get('/api/Players').success(function (data) {
+		$scope.players = data;
+	});
+}]);
+
+app.controller("CommentController", [ '$scope', '$http', function ($scope, $http) {
+	$scope.comments = [];
+	$scope.comment = {};
+	$scope.message = "n/a";
+
+	$http.get('/api/Comments').success(function (data) {
+		$scope.comments = data;
+		alert("comments loaded");
+	});
+
+	$scope.getComments = function () {
+		$http.get('/api/Comments').success(function (data) {
+			$scope.comments = data;
+		});
+	};
+
+	$scope.addComment = function (team) {
+		$scope.comment.TeamId = team.Id;
+		var comm = $scope.comment;
+		$http.post('/api/Comments', JSON.stringify(comm)).success(function (data) {
+			$scope.comment = {};
+			$scope.getComments();
+		});
+	};
+}]);
+
+app.controller("TabController", [ '$scope', function ($scope) {
+	$scope.tab = 1;
+		
+	$scope.selectTab = function (setTab) {
+		$scope.tab = setTab;
+	};
+
+	$scope.isSelected = function (checkTab) {
+		return $scope.tab === checkTab;
+	};
+}]);
